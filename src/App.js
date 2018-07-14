f5ece219cb600e78ae7f4b69bba4daeceb487b49
Kim Scarabello <kimberly.scarabello@gmail.com>
2018-07-14 12:49:34 -0700

    calculating fahrenheit correctly

--- src/App.js
+++ src/App.js
@@ -26,14 +26,15 @@ class App extends Component {
   }
   
   getLocationInfo(position) {
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}`)
-            .then(response => {
-              
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`)
+            .then(response => {              
+              let fahrenheit = response.data.main.temp * 1.8 + 32;
               this.setState({
                 description: response.data.weather[0].description,
-                fahrenheit: response.data.main.temp,
+                celsius: response.data.main.temp,
+                fahrenheit: fahrenheit,
                 image: response.data.weather[0].icon,
-                location: response.data.main.name
+                location: response.data.name
               })
           })
       }  
@@ -48,6 +49,7 @@ class App extends Component {
           <p>{this.state.location}</p>
           <img src={icon} />
           <p className="description">{this.state.description}</p>
+          <p>{this.state.celsius}</p>
           <p>{this.state.fahrenheit}</p>
         </div>
       </div>
